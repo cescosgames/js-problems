c7952febeb3a01522b9ea36b1508cc19769ff178
Cesco <cescosgames@gmail.com>
2026-03-06 23:07:06 -0500

    new problems

--- leetcodes/LC392.js
+++ leetcodes/LC392.js
@@ -3,6 +3,26 @@
 // Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 // A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., &quot;ace&quot; is a subsequence of &quot;abcde&quot; while &quot;aec&quot; is not).
 
+// this is a two pointer problem, so my initial idea is progress through both getting rid of non equal letters in order, once we reach end check equal
+var isSubsequence = function(s, t) {
+    // initialize our variables
+    let i = 0;
+    let j = 0;
+
+    // while i is less than s (first pointer) and j is less than 2 (second pointer)
+    while (i < s.length && j < t.length) {
+        if (s[i] === t[j]) { // if the letters are equal, increment both
+            i++;
+            j++;
+        } else { // otherwise only increment one side (j)
+            j++;
+        }
+    };
+
+    return i === s.length; // if i made it to the end, we are true, otherwise we are false
+};
+
+
 // Example 1:
 const s = "abc";
 const t = "ahbgdc";
@@ -12,6 +32,9 @@ const t = "ahbgdc";
 const s2 = "axc"
 const t2 = "ahbgdc"
 // Output: false
+
+console.log(isSubsequence(s, t))
+console.log(isSubsequence(s2, t2))
 
 // Constraints:
 // 	0 <= s.length <= 100
--- mess.js
+++ mess.js
@@ -1,35 +1,51 @@
-// LC 9 - Palindrome Number
+// LC 392 - Is Subsequence
 
-// Given an integer x, return true if x is a palindrome, and false otherwise.
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., &quot;ace&quot; is a subsequence of &quot;abcde&quot; while &quot;aec&quot; is not).
 
-var isPalindrome = function(x) {
-    // first instinct is just convert to string, palindrome check string
-    const reversedStr = x.toString().split('').reverse().join('')
+// this is a two pointer problem, so my initial idea is progress through both getting rid of non equal letters in order, once we reach end check equal
+var isSubsequence = function(s, t) {
+    // initialize our variables
+    let i = 0;
+    let j = 0;
 
-    console.log(reversedStr);
-    console.log(x.toString());
+    // while i is less than s (first pointer) and j is less than 2 (second pointer)
+    while (i < s.length && j < t.length) {
+        if (s[i] === t[j]) { // if the letters are equal, increment both
+            i++;
+            j++;
+        } else { // otherwise only increment one side (j)
+            j++;
+        }
+    };
 
-    if (reversedStr === x.toString()) return true;
-    return false
+    return i === s.length; // if i made it to the end, we are true, otherwise we are false
 };
 
+
 // Example 1:
-const x = 121
+const s = "abc";
+const t = "ahbgdc";
 // Output: true
-// Explanation: 121 reads as 121 from left to right and from right to left.
 
 // Example 2:
-const x2 = -121
+const s2 = "axc"
+const t2 = "ahbgdc"
 // Output: false
-// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
 
-// Example 3:
-const x3 = 10
-// Output: false
-// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+console.log(isSubsequence(s, t))
+console.log(isSubsequence(s2, t2))
 
-// Follow up: Could you solve it without converting the integer to a string?
+// Constraints:
+// 	0 <= s.length <= 100
+// 	0 <= t.length <= 104
+// 	s and t consist only of lowercase English letters.
 
-console.log(isPalindromeNoString(x))
-console.log(isPalindromeNoString(x2))
-console.log(isPalindromeNoString(x3))
+// Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+
+// Examples:
+// "abc"
+// "ahbgdc"
+// "axc"
+// "ahbgdc"
+
